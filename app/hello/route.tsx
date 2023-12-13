e69f550b254643664921fe1d25f09448628222fd
Danny <dannywo@gmail.com>
2023-12-12 23:16:27 -0800

    Updates to return a json response.

--- app/hello/route.tsx
+++ app/hello/route.tsx
@@ -1,3 +1,6 @@
+import { NextResponse } from "next/server"
+
 export async function GET() {
-    return new Response('testing routes.')
+    // return new Response('testing routes.')
+    return NextResponse.json({ message: 'heck yeah', count: 10 })
 }
